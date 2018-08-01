16349ab90f8a8e84595a0f28404d33265530e116
Demo User <demouser@MacBook-Pro.local>
2018-08-01 10:31:38 -0500

    IT FINALLY WORKS

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import webapp2
 import jinja2
 import air_con
+import logging
 
 template_loader = jinja2.FileSystemLoader(searchpath="./")
 template_env=jinja2.Environment(loader=template_loader)
-
-"""ac_dict = {"Trane XV20i TruComfort Variable Speed, Model 4TTV0024A": [22, 24000],
-    "Trane XV20i TruComfort Variable Speed, Model 4TTV0036A": [22, 36000],
-    "Trane XV20i TruComfort Variable Speed, Model 4TTV0048A": [22, 48000],
-    "Trane XV20i TruComfort Variable Speed, Model 4TTV0060A": [22, 60000]}
-states_dict = {'Texas': 10.98}"""
 
 def avgthese(a, b):
     one = int(a)
@@ -54,19 +49,34 @@ class RegionPage(webapp2.RequestHandler):
 class AppliancePage(webapp2.RequestHandler):
     def get(self):
         beg_template = template_env.get_template('templates/app-results.html')
-        self.response.write(beg_template.render())
+        ac_arr=[]
+        states_arr=[]
+        for pair in sorted(air_con.ac_dict.items()):
+            ac_arr.append(pair[0])
+        for pair in sorted(air_con.states_dict.items()):
+            states_arr.append(pair[0])
+        dict = {'dict': ac_arr, 's_dict': states_arr, 'my_dict':{}}
+        self.response.write(beg_template.render(dict))
 
     def post(self):
         self.response.headers['Content-Type'] = 'text/html'
         ac=str(self.request.get('ac-type'))
         ac2=str(self.request.get('ac-type-2'))
         state = str(self.request.get('state'))
+        logging.info(ac)
+        ac_arr=[]
+        states_arr=[]
+        for pair in sorted(air_con.ac_dict.items()):
+            ac_arr.append(pair[0])
+        for pair in sorted(air_con.states_dict.items()):
+            states_arr.append(pair[0])
+        dict = {'dict': ac_arr, 's_dict': states_arr}
         seer = air_con.ac_dict[ac][0]
         size = air_con.ac_dict[ac][1]
         seer2 = air_con.ac_dict[ac2][0]
         size2 = air_con.ac_dict[ac2][1]
         money = air_con.states_dict[state]
-        my_dict = {'name1': ac, 'name2': ac2, 'state': state, 'seer': seer, 'size': size, 'money': money, 'seer2': seer2, 'size2': size2}
+        my_dict = {'dict': ac_arr, 's_dict': states_arr, 'my_dict':{'name1': ac, 'name2': ac2, 'state': state, 'seer': seer, 'size': size, 'money': money, 'seer2': seer2, 'size2': size2}}
         end_template = template_env.get_template('templates/app-results.html')
         self.response.write(end_template.render(my_dict))
 
